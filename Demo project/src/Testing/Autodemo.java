9cfc2545b56f42f590ca4b010de8c3666da05ad0
dexbytes <dexbytes@192.168.0.123>
2019-01-16 15:14:41 +0530

    New addon

--- Demo project/src/Testing/Autodemo.java
+++ Demo project/src/Testing/Autodemo.java
@@ -1,5 +1,16 @@
 package Testing;
 
-public class Autodemo {
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
+public class Autodemo {
+	
+	public static class ChangeOut {
+		public static void main(String args[]) {
+		try {
+		System.setOut(new PrintStream(new FileOutputStream("log.txt")));
+		System.out.println("Now the output is redirected!");
+		} catch(Exception e) {}
+		}
+		}
 }
